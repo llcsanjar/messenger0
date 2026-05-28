d385865a9c3a7223217e06bec51e2ce9811b34f9
truesanjar <truesanjar@gmail.com>
2026-05-28 21:14:20 +0500

    first commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js")
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBEfyEUvDNqlrNj5He4joklzMSwjITxwKE",
-    authDomain: "messanger-0.firebaseapp.com",
-    projectId: "messanger-0",
-    storageBucket: "messanger-0.firebasestorage.app",
-    messagingSenderId: "321068488797",
-    appId: "1:321068488797:web:6d6eabc2584ecc5e6155b4",
-    measurementId: "G-JWPG5WXY0K"
+    apiKey: "AIzaSyAjdcn4j-d8DjF62rNfpD3yLqma5rGcRYg",
+    authDomain: "messenger0-1b3e9.firebaseapp.com",
+    projectId: "messenger0-1b3e9",
+    storageBucket: "messenger0-1b3e9.firebasestorage.app",
+    messagingSenderId: "613278078220",
+    appId: "1:613278078220:web:c20cf100955bf164eca529",
+    measurementId: "G-KSDDYNFJDF"
 })
 
 const messaging = firebase.messaging()
